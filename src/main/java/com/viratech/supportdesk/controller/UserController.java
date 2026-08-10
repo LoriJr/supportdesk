6bac0acdc21b39fd11bed90e7d236f32739ef664
lorijr <jrsilva.lorivas@gmail.com>
2026-08-10 08:54:50 -0300

    chore: adicionado validações na request

--- src/main/java/com/viratech/supportdesk/controller/UserController.java
+++ src/main/java/com/viratech/supportdesk/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.viratech.supportdesk.controller;
 
-import com.viratech.supportdesk.domain.User;
 import com.viratech.supportdesk.dto.UserRequest;
 import com.viratech.supportdesk.dto.UserResponse;
 import com.viratech.supportdesk.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -16,7 +19,7 @@ public class UserController {
     private final UserService service;
 
     @PostMapping
-    public ResponseEntity<UserResponse> save(@RequestBody UserRequest request){
+    public ResponseEntity<UserResponse> save(@Valid @RequestBody UserRequest request){
         UserResponse response = service.saveUser(request);
         return ResponseEntity.ok(response);
     }
--- src/main/java/com/viratech/supportdesk/dto/UserRequest.java
+++ src/main/java/com/viratech/supportdesk/dto/UserRequest.java
@@ -1,7 +1,15 @@
 package com.viratech.supportdesk.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UserRequest(
+
+        @NotBlank(message = "user must not be null!")
         String name,
+
+        @NotBlank(message = "email must not be null!")
+        @Email
         String email
 ) {
 }
