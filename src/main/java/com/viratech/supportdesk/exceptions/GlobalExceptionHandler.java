3e3e20a52c7bed1c8ffc765e07ffc1e68ef2d884
lorijr <jrsilva.lorivas@gmail.com>
2026-09-12 00:23:03 -0300

    feat: add exceptions of the conflict email already exists

---
+++ src/main/java/com/viratech/supportdesk/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.viratech.supportdesk.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<String> handleEmailAlreadyExists(EmailAlreadyExistsException ex){
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(ex.getMessage());
+    }
+}
--- src/main/java/com/viratech/supportdesk/repository/UserRepository.java
+++ src/main/java/com/viratech/supportdesk/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.viratech.supportdesk.repository;
 import com.viratech.supportdesk.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
 }
